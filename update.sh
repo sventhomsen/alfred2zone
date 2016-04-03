962abc0a1e5a036b23323a882b9c039a42c7e5be
Sven Thomsen <sven@tumelum.de>
2016-04-03 21:08:07 +0200

    Initial change for ffnord

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LC_ALL=en_US.UTF-8
-curl https://map.luebeck.freifunk.net/data/nodes.json | /usr/bin/python3 /usr/local/bin/alfred2zone/alfred2zone.py > /var/cache/bind/nodes.ffhl.zone
+curl https://vpn3.ffnord/ffmap-backend-data/nodes.json | /usr/bin/python3 /usr/local/bin/alfred2zone.py > /etc/bind/zones/nodes.ffnord.zone
 /usr/sbin/rndc reload
